323af816f7e0122a2e3649b1ff15f90e8a3264a5
Ivan Samardzic <ivan.samardzic7@gmail.com>
2024-01-22 13:53:57 -0500

    Added mdr

---
+++ mdr.v
@@ -0,0 +1,20 @@
+module Data (
+	//MDMux
+	input [31:0]BusMuxOut, input [31:0]Mdatain, Read
+	//MDR
+	input Clear, Clock, MDRin,
+
+	output wire [31:0]MDMuxout,
+	wire [31:0] MDRout register (Clock, MDRin, CLear, MDMuxout)  
+
+);
+
+reg [31:0]q;
+
+always @ (*) begin
+	if(Read) MDMuxout = Mdatain;
+	else 
+	MDMuxout = BusMuxOut;
+
+end
+endmodule
