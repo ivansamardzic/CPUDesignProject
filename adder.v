34d459df2fe62ce96c7208c42c0c788f31ae2280
TaesonTitus <114195814+TaesonTitus@users.noreply.github.com>
2024-01-28 17:29:54 -0500

    Update adder.v
    4 bit ripple for carry select

--- adder.v
+++ adder.v
@@ -1,21 +1,24 @@
 // Ripple Carry Adder
-module adder(A, B, Result);
+module RippleCarryAdder_4bit(A, B, Result, c_out);
 
-input [7:0] A, B;
-output [7:0] Result;
+	input [3:0] A, B;
+	output [3:0] Result;
+	output c_out; 
 
-reg [7:0] Result;
-reg [8:0] LocalCarry;
+	reg [3:0] Result;
+	reg [4:0] LocalCarry;
 
 integer i;
 
 always@(A or B)
 	begin
-		LocalCarry = 9'd0;
-		for(i = 0; i < 8; i = i + 1)
+		LocalCarry = 5'd0;
+		for(i = 0; i < 4; i = i + 1)
 		begin
-				Result[i] = A[i]^B[i]^LocalCarry[i];
-				LocalCarry[i+1] = (A[i]&B[i])|(LocalCarry[i]&(A[i]|B[i]));
+			//^ = xor
+			Result[i] = A[i]^B[i]^LocalCarry[i];
+			LocalCarry[i+1] = (A[i]&B[i]) | (LocalCarry[i]&A[I]) | (LocalCarry[i]&B[i]);
 		end
-end
-endmodule
+	end
+	c_out = LocalCarry[4]
+endmodule 
