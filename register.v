ba56aa69395670a560709d59071e3518e0855060
Ivan Samardzic <ivan.samardzic7@gmail.com>
2024-01-29 12:09:55 -0500

    CHanged to 32 bit

--- register.v
+++ register.v
@@ -1,4 +1,4 @@
-module register #(parameter DATA_WIDTH_IN = 8, DATA_WIDTH_OUT = 8, INIT = 8'h0)(
+module register #(parameter DATA_WIDTH_IN = 32, DATA_WIDTH_OUT = 32, INIT = 32'h0)(
 	input clear, clock, enable, 
 	input [DATA_WIDTH_IN-1:0]BusMuxOut,
 	output wire [DATA_WIDTH_OUT-1:0]BusMuxIn
