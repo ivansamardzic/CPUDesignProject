21bce23c2bcdb0e16e180cbed62c688e91531fc6
TaesonTitus <114195814+TaesonTitus@users.noreply.github.com>
2024-02-07 00:18:42 -0500

    Update MDR.v
    name change

--- MDR.v
+++ MDR.v
@@ -1,12 +1,12 @@
 module MDR(
 	input clear, clock, MDRin,
-	input [32-1:0]BusMuxOut, Mdatain, //MDR register has extra Mdatain input
+	input [31:0]BMout, Mdatain, //MDR register has extra Mdatain input
 	input read,												// extra signal for MDMux select, see Phase 1 documentation
 	
-	output wire [32-1:0]BusMuxIn 		// '02/01', output to memory chip should prob also be here 
+	output wire [31:0]BMInMDR		// '02/01', output to memory chip should prob also be here 
 );
 
-reg [32-1:0]q;
+reg [31:0]q;
 
 initial q = 8'h00000002;
 
@@ -17,9 +17,9 @@ always @ (posedge clock)
 			end
 			else if (MDRin) begin
 				if(read) q <= Mdatain; //assuming read=1 means read from mem chip
-				else q <= BusMuxOut; // otherwise set q to data from bus
+				else q <= BMout; // otherwise set q to data from bus
 			end
 		end
 		
-	assign BusMuxIn = q[32-1:0];
+	assign BMInMDR = q[31:0];
 endmodule
