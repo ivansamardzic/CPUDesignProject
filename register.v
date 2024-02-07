4459c9ee5159618842c1c812d030a55497eea101
TaesonTitus <114195814+TaesonTitus@users.noreply.github.com>
2024-02-07 00:21:59 -0500

    Update register.v
    name change

--- register.v
+++ register.v
@@ -1,7 +1,7 @@
 module register #(parameter DATA_WIDTH_IN = 32, DATA_WIDTH_OUT = 32, INIT = 32'h0)(
 	input clear, clock, enable, 
-	input [DATA_WIDTH_IN-1:0]BusMuxOut,
-	output wire [DATA_WIDTH_OUT-1:0]BusMuxIn
+	input [DATA_WIDTH_IN-1:0]BMout,
+	output wire [DATA_WIDTH_OUT-1:0]BMIn
 );
 reg [DATA_WIDTH_IN-1:0]q;
 initial q = INIT;
@@ -11,8 +11,8 @@ always @ (posedge clock)
 				q <= {DATA_WIDTH_IN{1'b0}};
 			end
 			else if (enable) begin
-				q <= BusMuxOut;
+				q <= BMout;
 			end
 		end
-	assign BusMuxIn = q[DATA_WIDTH_OUT-1:0];
+	assign BMIn = q[DATA_WIDTH_OUT-1:0];
 endmodule
