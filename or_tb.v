658186b15da716b1d380958c56c2e3724485f531
TaesonTitus <114195814+TaesonTitus@users.noreply.github.com>
2024-02-10 21:52:33 -0500

    Update or_tb.v
    feb 10 work

--- or_tb.v
+++ or_tb.v
@@ -1,10 +1,9 @@
-//Needs work -----> Using AND_tb to implement this tb
 `timescale 1ns/10ps
 module or_tb;
-    reg PCout, Zlowout, MDRout, R2out, R3out; // add any other signals to see in your simulation
-    reg MARin, Zin, PCin, MDRin, IRin, Yin;
-    reg IncPC, Read, AND, R1in, R2in, R3in;
-    reg Clock;
+    reg PCout, Zlowout, MDRout, R2out, R3out, R4out; // add any other signals to see in your simulation
+    reg MARin, Zlowin, PCin, MDRin, IRin, Yin;
+    reg IncPC, Read, OR, R1in, R2in, R3in, R4in;
+    reg clock, clear;
     reg [31:0] Mdatain;
 
     parameter   Default = 4'b0000, Reg_load1a = 4'b0001, Reg_load1b = 4'b0010, Reg_load2a = 4'b0011,
@@ -12,31 +11,37 @@ module or_tb;
                 T1 = 4'b1000, T2 = 4'b1001, T3 = 4'b1010, T4 = 4'b1011, T5 = 4'b1100;
     reg [3:0] Present_state = Default;
 
-Datapath DUT(PCout, Zlowout, MDRout, R2out, R3out, MARin, Zin, PCin, MDRin, IRin, Yin, IncPC, Read, AND, R1in,
-R2in, R3in, Clock, Mdatain);
+	DataPath DUT(.clock(clock), .clear(clear), 
+		     .R1in(R1in), .R2in(R2in), .R3in(R3in), .R4in(R4in),
+			  .R2out(R2out), .R3out(R3out),  
+		     .MDRin(MDRin), 
+		     .MARin(MARin), .PCin(PCin), .MD_read(Read),
+		     .Zlowin(Zlowin), .Zlowout(Zlowout), .IncPC(IncPC), .Yin(Yin), .IRin(IRin),  
+		     .Mdatain(Mdatain), .MDRout(MDRout));
 // add test logic here
-initial
-    begin
-        Clock = 0;
-        forever #10 Clock = ~ Clock;
-end
+		
+	initial
+		begin
+		   clock = 0;
+			clear = 0;
+		   forever #10 clock = ~ clock;
+		end
 
-
-always @(posedge Clock) // finite state machine; if clock rising-edge
+always @(posedge clock) // finite state machine; if clock rising-edge
     begin
         case (Present_state)
-            Default : Present_state = Reg_load1a;
-            Reg_load1a : Present_state = Reg_load1b;
-            Reg_load1b : Present_state = Reg_load2a;
-            Reg_load2a : Present_state = Reg_load2b;
-            Reg_load2b : Present_state = Reg_load3a;
-            Reg_load3a : Present_state = Reg_load3b;
-            Reg_load3b : Present_state = T0;
-            T0 : Present_state = T1;
-            T1 : Present_state = T2;
-            T2 : Present_state = T3;
-            T3 : Present_state = T4;
-            T4 : Present_state = T5;
+            Default : #40 Present_state = Reg_load1a;
+            Reg_load1a : #40 Present_state = Reg_load1b;
+            Reg_load1b : #40 Present_state = Reg_load2a;
+            Reg_load2a : #40 Present_state = Reg_load2b;
+            Reg_load2b : #40 Present_state = Reg_load3a;
+            Reg_load3a : #40 Present_state = Reg_load3b;
+            Reg_load3b : #40 Present_state = T0;
+            T0 : #40 Present_state = T1;
+            T1 : #40 Present_state = T2;
+            T2 : #40 Present_state = T3;
+            T3 : #40 Present_state = T4;
+            T4 : #40 Present_state = T5;
         endcase
     end
 
@@ -45,60 +50,76 @@ always @(Present_state) // do the required job in each state
         case (Present_state) // assert the required signals in each clock cycle
             Default: begin
                     PCout <= 0; Zlowout <= 0; MDRout <= 0; // initialize the signals
-                    R2out <= 0; R3out <= 0; MARin <= 0; Zin <= 0;
+                    R2out <= 0; R3out <= 0; MARin <= 0; Zlowin <= 0;
                     PCin <=0; MDRin <= 0; IRin <= 0; Yin <= 0;
-                    IncPC <= 0; Read <= 0; AND <= 0;
+                    IncPC <= 0; Read <= 0; OR <= 0;
                     R1in <= 0; R2in <= 0; R3in <= 0; Mdatain <= 32'h00000000;
             end
+		//------------------------------------------------------------
             Reg_load1a: begin
-                    Mdatain <= 32'h00000012;
-                    Read = 0; MDRin = 0; // the first zero is there for completeness
-                    #10 Read <= 1; MDRin <= 1;
-                    #15 Read <= 0; MDRin <= 0;
+			Mdatain <= 32'h00000012; //sends data to MDMux
+			Read = 0; MDRin = 0; // does nothing
+			#10 Read <= 1; MDRin <= 1; //sets BusMuxInMDR to Mdatain 
+			#15 Read <= 0; MDRin <= 0; //read selects Busmux out, MDRin disables MDR
             end
             Reg_load1b: begin
-                    #10 MDRout <= 1; R2in <= 1;
-                    #15 MDRout <= 0; R2in <= 0; // initialize R2 with the value $12
+			#10 MDRout <= 1; R2in <= 1; //MDRout = 1 sets BusMuxOut to BusMuxInMDR aka Mdatain
+			//R2in = 1 enables R2in, sets BMInR2 to BusMuxOut 
+			#15 MDRout <= 0; R2in <= 0; //MDRout dissables bus change, R2in dissables register R2 change
             end
+		//------------------------------------------------------------
             Reg_load2a: begin
                     Mdatain <= 32'h00000014;
                     #10 Read <= 1; MDRin <= 1;
                     #15 Read <= 0; MDRin <= 0;
+						  //puts Mdatain to BusMuxInMDR
             end
             Reg_load2b: begin
                     #10 MDRout <= 1; R3in <= 1;
                     #15 MDRout <= 0; R3in <= 0; // initialize R3 with the value $14
+						  //puts BusMuxInMDR to BusMuxOut to Register 3 (BMInR3)
             end
+		//------------------------------------------------------------
             Reg_load3a: begin
                     Mdatain <= 32'h00000018;
                     #10 Read <= 1; MDRin <= 1;
                     #15 Read <= 0; MDRin <= 0;
+							//puts Mdatain to BusMuxInMDR
             end
             Reg_load3b: begin
                     #10 MDRout <= 1; R1in <= 1;
                     #15 MDRout <= 0; R1in <= 0; // initialize R1 with the value $18
+						  //puts BusMuxInMDR to BusMuxOut to Register 1 (BMInR1)
             end
-
-
+		//------------------------------------------------------------
+		
             T0: begin // see if you need to de-assert these signals
-                    PCout <= 1; MARin <= 1; IncPC <= 1; Zin <= 1;
+                    #10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1;
+		    #15 PCout <= 0; MARin <= 0; IncPC <= 0; Zlowin <= 0;
+						  
             end
             T1: begin
-                    Zlowout <= 1; PCin <= 1; Read <= 1; MDRin <= 1;
-                    Mdatain <= 32'h28918000; // opcode for “and R1, R2, R3”
+                    #10 Zlowout <= 1; PCin <= 1; Read <= 1; MDRin <= 1;
+                    Mdatain <= 4'b0001; // opcode for “and R1, R2, R3”
+		    #15 Zlowout <= 0; PCin <= 0; Read <= 0; MDRin <= 0;
             end
             T2: begin
-                    MDRout <= 1; IRin <= 1;
+                    #10 MDRout <= 1; IRin <= 1; //transfers MDR contents to IR reg
+		    #15 MDRout <= 0; IRin <= 0;
             end
             T3: begin
-                    R2out <= 1; Yin <= 1;
+                    #10 R2out <= 1; Yin <= 1; //transfers R2 contents to Y reg
+			#15 R2out <= 0; Yin <= 0;
             end
             T4: begin
-                    R3out <= 1; OR <= 1; Zin <= 1;
+                    #10 R3out <= 1; OR <= 1; Zlowin <= 1; //transfers R3 to bus, does OR, Z takes in OR result
+		    #15 R3out <= 0; OR <= 0; Zlowin <= 0;
             end
             T5: begin
-                    Zlowout <= 1; R1in <= 1;
+                    #10 Zlowout <= 1; R1in <= 1; //transfers zlow contents into R1 
+		    #15 Zlowout <= 0; R1in <= 0;
             end
         endcase
     end
 endmodule
+
