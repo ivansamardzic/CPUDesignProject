20a0833fa5d587d4df56a2793d31f63bf667c05b
Ivan Samardzic <21is8@queensu.ca>
2024-02-12 17:11:16 -0500

    rol works

--- rol_32_bit.v
+++ rol_32_bit.v
@@ -1,4 +1,5 @@
-module rol_32_bit(input wire[31:0] in, roll, output wire[31:0] out);
-    shl_32_bit sh(in, roll, out);
-    assign out [roll:0] = in [31:31 - roll]; 
+module rol_32_bit(input wire[31:0] in, input wire[31:0] amount, output wire[31:0] out);
+
+	assign out = (in << amount) | (in >> (32 - amount));
+	
 endmodule
