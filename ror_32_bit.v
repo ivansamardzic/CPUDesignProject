dd8533924a5a1c90ae50327d1e34f5cfdb95a2bf
Ivan Samardzic <21is8@queensu.ca>
2024-02-12 14:58:08 -0500

    ror

--- ror_32_bit.v
+++ ror_32_bit.v
@@ -1,4 +1,7 @@
-module ror_32_bit(input wire [31:0] in, reg roll, output wire [31:0] out);
-    shr_32_bit sh(in, roll, out); 
-    assign out [31:31 - roll] = in [roll:0]; 
+module ror_32_bit(input wire [31:0] in, input wire[31:0] roll, output wire [31:0] out);
+
+	 
+    assign out = (in >> roll) | { (in << (32 - roll))[31:0], {32-roll{1'b0}} };
+
+
 endmodule
