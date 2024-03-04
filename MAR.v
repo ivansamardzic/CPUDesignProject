a1df5be89f0a694c75b06a03c6937ad8f5e92459
Ivan Samardzic <ivan.samardzic7@gmail.com>
2024-03-04 11:55:53 -0500

    MAR complete

---
+++ MAR.v
@@ -0,0 +1,18 @@
+module MAR(
+    input wire clear, clock, MARIn,
+    input wire [31:0] BusMuxOut,
+    output wire [8:0] MAROut
+);
+    
+        reg [8:0] q;
+    
+        always @(posedge clock) begin
+            if(clear) begin
+                q <= 9'b0;
+            end
+            else if (MARIn) begin
+                q <= BusMuxOut[8:0];
+            end
+        end
+        assign MAROut = q;
+endmodule
