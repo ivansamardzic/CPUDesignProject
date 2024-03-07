079474f36ff4024857aacdae68adac0e92016fc6
TaesonTitus <114195814+TaesonTitus@users.noreply.github.com>
2024-03-07 15:04:40 -0500

    Update DataPath.v

--- DataPath.v
+++ DataPath.v
@@ -3,40 +3,46 @@
 module DataPath(
 	input wire clock, clear,
 	
-	input wire HIin, LOin, Zlowin, Zhighin, PCin, MDRin, InPortin, Cin, MD_read,
+	input wire HIin, LOin, Zlowin, Zhighin, PCin, MDRin, MARin, InPortin, Cin, MD_read,
 	input wire IncPC, IRin, OutPortin, Yin, 
 	input wire Out_Portin, Strobe, 
-	input wire MARIn,
-
-	input wire HIout, LOout, Zhighout, Zlowout, PCout, MDRout, MARout, InPortout, Cout,
 	
-	input wire Gra, Grb, Grc, Rin, Rout, BAout, 
+	input wire HIout, LOout, Zhighout, Zlowout, PCout, MDRout, MARout, InPortout, 
+	
+	input wire Gra, Grb, Grc, Rin, Rout, BAout, Csignout, Read, Write, 
+	input wire ADD, 
 	
 	//change to outs later
    //input wire Zin, IncPC, IRin,  OutPortin,  Yin,  	
-	input wire [31:0] Mdatain,
-	input wire [31:0] INPUT_UNIT,
-	input wire [31:0] OUTPUT_UNIT,
-	input wire [8:0] address
+	input wire [31:0] Mdatain
+	//input wire [31:0] INPUT_UNIT,
+	//input wire [31:0] OUTPUT_UNIT
 );
 
 	wire [31:0] BMInR0, BMInR1, BMInR2, BMInR3, BMInR4, BMInR5, BMInR6, BMInR7, BMInR8, BMInR9, BMInR10, BMInR11, BMInR12, BMInR13, BMInR14, BMInR15; 
 	wire [31:0] BMInINPORT;
-	wire [31:0] BusMuxOut, C_sign_extended, BMInPC, BMInIR, BusMuxInMDR, BMInCSign, BMInHI, BMInLO, BMInZhigh, BMInZlow, BMInY, BMInMAR; 
+	wire [31:0] BusMuxOut, C_sign_extended, BMInPC, BMInIR, BusMuxInMDR, BusMuxInMAR, BMInHI, BMInLO, BMInZhigh, BMInZlow, BMInY; 
 	wire [63:0] C; 
 	
 	wire [15:0] IN, OUT; 
-
+	wire [8:0] address;
+	
+	
 	
 	//MDR mdr(BuxMuxOut, Mdatain, MD_read, clear, clock, MDRin, MDRout);
 	MDR mdr(.clear(clear), .clock(clock), .MDRin(MDRin), .BusMuxOut(BusMuxOut), .Mdatain(Mdatain), .read(MD_read), .BusMuxInMDR(BusMuxInMDR));
 	
 	register IR_reg(clear, clock, IRin, BusMuxOut, BMInIR); 
+	
 	Select_Encode E1(.BMInIR(BMInIR), .Gra(Gra), .Grb(Grb), .Grc(Grc), .Rin(Rin), .Rout(Rout), .BAout(BAout), .IN(IN), .OUT(OUT), .C_sign_extended(C_sign_extended)); 
 	
 	I_O E2(.clear(clear), .clock(clock), .Out_Portin(Out_Portin), .Strobe(Strobe), .BusMuxOut(BusMuxOut), .OUTPUT_UNIT(OUTPUT_UNIT), .INPUT_UNIT(INPUT_UNIT), .BMInINPORT(BMInINPORT));
 	
-	MAR mar(.clear(clear), .clock(clock), .MARIn(MARIn), .BusMuxOut(BusMuxOut), .address(address));
+	MAR mar(.clear(clear), .clock(clock), .MARin(MARin), .BusMuxOut(BusMuxOut), .address(address));
+	
+	RAM ram(.Read(Read), .Write(Write), .clock(clock), .DataIn(BusMuxOut), .DataOut(Mdatain));  
+	
+	
 	//Register Assignment
 	//clear, clock, enable, input, output 
 	
@@ -56,7 +62,6 @@ module DataPath(
 				end
 			end
 		assign BMInR0 = q & ~BAout; //may need to concatenate 
-		
 		
 		
 	register R1(clear, clock, IN[1], BusMuxOut, BMInR1);
@@ -89,15 +94,25 @@ module DataPath(
 	
 	Bus bus(
 		.C_sign_extended(C_sign_extended), .Strobe(Strobe), .BMInINPORT(BMInINPORT), 
-		.BMInR0(BMInR0), .BMInR1(BMInR1), .BMInR2(BMInR2), .BMInR3(BMInR3), .BMInR4(BMInR4), .BMInR5(BMInR5), .BMInR6(BMInR6), .BMInR7(BMInR7), .BMInR8(BMInR8), .BMInR9(BMInR9), .BMInR10(BMInR10), .BMInR11(BMInR11), .BMInR12(BMInR12), .BMInR13(BMInR13), .BMInR14(BMInR14), .BMInR15(BMInR15),
-		.BMInHI(BMInHI), .BMInLO(BMInLO), .BMInZhigh(BMInZhigh), .BMInZlow(BMInZlow), .BMInPC(BMInPC), .BusMuxInMDR(BusMuxInMDR), .BMInInPort(BMInInPort), .BMInCSign(BMInCSign),
-
-		.R0out(OUT[0]), .R1out(OUT[1]), .R2out(OUT[2]), .R3out(OUT[3]), .R4out(OUT[4]), .R5out(OUT[5]), .R6out(OUT[6]), .R7out(OUT[7]), .R8out(OUT[8]), .R9out(OUT[9]), .R10out(OUT[10]), .R11out(OUT[11]), .R12out(OUT[12]), .R13out(OUT[13]), .R14out(OUT[14]), .R15out(OUT[15]),
-		.HIout(HIout), .LOout(LOout), .Zhighout(Zhighout), .Zlowout(Zlowout), .PCout(PCout), .MDRout(MDRout), .InPortout(InPortout), .Cout(Cout),
 		
-		.BusMuxOut(BusMuxOut), .BMInMAR(BMInMAR)
+		.BMInR0(BMInR0), .BMInR1(BMInR1), .BMInR2(BMInR2), .BMInR3(BMInR3), .BMInR4(BMInR4), .BMInR5(BMInR5), .BMInR6(BMInR6), .BMInR7(BMInR7), .BMInR8(BMInR8), .BMInR9(BMInR9), .BMInR10(BMInR10), .BMInR11(BMInR11), .BMInR12(BMInR12), .BMInR13(BMInR13), .BMInR14(BMInR14), .BMInR15(BMInR15),
+		.BMInHI(BMInHI), .BMInLO(BMInLO), .BMInZhigh(BMInZhigh), .BMInZlow(BMInZlow), .BMInPC(BMInPC), .BusMuxInMDR(BusMuxInMDR), .BMInInPort(BMInInPort),
+
+		.Csignout(Csignout),
+		
+		.R0out(OUT[0]), .R1out(OUT[1]), .R2out(OUT[2]), .R3out(OUT[3]), .R4out(OUT[4]), .R5out(OUT[5]), .R6out(OUT[6]), .R7out(OUT[7]), .R8out(OUT[8]), .R9out(OUT[9]), .R10out(OUT[10]), .R11out(OUT[11]), .R12out(OUT[12]), .R13out(OUT[13]), .R14out(OUT[14]), .R15out(OUT[15]),
+		.HIout(HIout), .LOout(LOout), .Zhighout(Zhighout), .Zlowout(Zlowout), .PCout(PCout), .MDRout(MDRout), .InPortout(InPortout),
+		
+		.BusMuxOut(BusMuxOut)
 		);
+		
 	
-	ALU alu(.Y(BMInY), .BusMuxOut(BusMuxOut), .op(BMInIR), .C(C));
+		ALU alu(.Y(BMInY), .BusMuxOut(BusMuxOut), .op(4'b0100), .enable(ADD), .C(C));
+
+	
+	
+		//ADD LOGIC_____________________________________________________________ call alu later but im lazy 
+	
+	
 endmodule
 
