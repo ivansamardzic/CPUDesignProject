b175dc2b9669c7b07b3809cfe00ff9491993f966
TaesonTitus <114195814+TaesonTitus@users.noreply.github.com>
2024-03-11 21:02:14 -0400

    Update ldi_tb.v
    loads 0x93 into R2 check ram

--- ldi_tb.v
+++ ldi_tb.v
@@ -3,23 +3,23 @@ module ldi_tb;
     reg PCout, Zlowout, MDRout; // add any other signals to see in your simulation
     reg Zlowin, PCin, MDRin, IRin, Yin;
     reg IncPC, ADD;
-    reg clock, clear;
-    reg [31:0] Mdatain;
+    reg clock, clear, MD_read, MAR_clear;
+//    reg [31:0] Mdatain;
 
     parameter   Default = 4'b0000, T0 = 4'b0001, T1 = 4'b0010, T2 = 4'b0011, T3 = 4'b0100, 
-	 T4 = 4'b0101, T5 = 4'b0110, T6 = 4'b0111, T7 = 4'b1000;
+	 T4 = 4'b0101, T5 = 4'b0110, T6 = 4'b0111, T7 = 4'b1000, T8 = 4'b1001; 
     reg [3:0] Present_state = Default;
 	 
 	 reg Csignout, Grb, Gra, Read, MARin, Rin, BAout; 
 	 
 
 	DataPath D(.clock(clock), .clear(clear),  
-		     .MDRin(MDRin), 
-		     .PCin(PCin), .MD_read(Read),
+		     .MDRin(MDRin), .MAR_clear(MAR_clear),
+		     .PCin(PCin), .MD_read(MD_read),
 		     .Zlowin(Zlowin), .Zhighin(Zhighin), .Zlowout(Zlowout), .IncPC(IncPC), .Yin(Yin), .IRin(IRin),  
-		     .Mdatain(Mdatain), .MDRout(MDRout), 
+		     .MDRout(MDRout), 
 			  
-			  .Csignout(Csignout), .Grb(Grb), .Gra(Gra), .Read(Read), .MARin(MARin), .Rin(Rin), .ADD(ADD));
+			  .Csignout(Csignout), .Grb(Grb), .Gra(Gra), .BAout(BAout), .Read(Read), .MARin(MARin), .Rin(Rin), .ADD(ADD));
 // add test logic here
 		
 	initial
@@ -40,7 +40,7 @@ always @(posedge clock) // finite state machine; if clock rising-edge
             T4 : #40 Present_state = T5;
 				T5 : #40 Present_state = T6;
             T6 : #40 Present_state = T7;
-            
+            T7 : #40 Present_state = T8;
         endcase
     end
 
@@ -52,43 +52,38 @@ always @(Present_state)
                     MARin <= 0; Zlowin <= 0;
                     PCin <=0; MDRin <= 0; IRin <= 0; Yin <= 0;
                     IncPC <= 0; Read <= 0; ADD <= 0;
-                    Mdatain <= 32'h00000000;
-						  Csignout <= 0; Grb <= 0; Gra <= 0; BAout <= 0; Rin <= 0; 
+                     MD_read <= 0;
+						  Csignout <= 0; Grb <= 0; Gra <= 0; BAout <= 0; Rin <= 0; MAR_clear <= 1;
             end
 		
-            T0: begin // see if you need to de-assert these signals
-						#10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1;
+            T0: begin //Puts PC into MAR 
+						#10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1; 
 						#15 PCout <= 0; MARin <= 0; IncPC <= 0; Zlowin <= 0;
             end
-            T1: begin
-						#10 Zlowout <= 1; PCin <= 1; Read <= 1; MDRin <= 1; Mdatain <= 32'b00001_0001_0000_0000000000000000000; 
-						#15 Zlowout <= 0; PCin <= 0; Read <= 0; MDRin <= 0;
+            T1: begin //Puts ram data into Mdatain
+						#10 Zlowout <= 1; PCin <= 1; Read <= 1; MDRin <= 1; MD_read <= 1;  
+						#15 Zlowout <= 0; PCin <= 0; Read <= 0; MDRin <= 0; MD_read <= 0;
             end
-            T2: begin
-                  #10 MDRout <= 1; IRin <= 1; //transfers MDR contents to IR reg
-						#15 MDRout <= 0; IRin <= 0;
+            T2: begin //MDR content on to bus
+                  #10 MDRout <= 1; MD_read <= 1; MDRin <= 1; 
+						#15 MDRout <= 0; MD_read <= 0; MDRin <= 0;
             end
-            T3: begin
-                  #10 Grb <= 1; BAout <= 1; Yin <= 1; //R2 contents get put into Yreg
+				T3: begin //IR has opcode 
+                  #10 IRin <= 1;  
+						#15 IRin <= 0; 
+            end
+            T4: begin //Yin contains 0 from R0
+                  #10 Grb <= 1; BAout <= 1; Yin <= 1; 
 						#15 Grb <= 0; BAout <= 0; Yin <= 0; 
             end
-            T4: begin
+            T5: begin
                   #10 Csignout <= 1; ADD <= 1; Zlowin <= 1; //contents in Creg + Yreg = ZlowReg
 						#15 Csignout <= 0; ADD <= 0; Zlowin <= 0; 
             end
-            T5: begin
-                  #10 Zlowout <= 1; MARin <= 1; //Zreg = address 
-						#15 Zlowout <= 0; MARin <= 0;
-            end
-				T6: begin 
-						#10 Read <= 1; MDRin <= 1; 
-						#15 Read <= 0; MDRin <= 0;
-				end
-				T7: begin
-						#10 MDRout <= 1; Gra <= 1; Rin <= 1; //In register 1
-						#15 MDRout <= 0; Gra <= 0; Rin <= 0; 
+				T6: begin
+						#10 Zlowout <= 1; Gra <= 1; Rin <= 1; //In register 2
+						#15 Zlowout <= 0; Gra <= 0; Rin <= 0; 
 				end
         endcase
     end
 endmodule
-
