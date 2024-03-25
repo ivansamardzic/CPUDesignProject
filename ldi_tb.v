babefbb35e0b8332327349e7c8d8c031ba625297
TaesonTitus <114195814+TaesonTitus@users.noreply.github.com>
2024-03-25 13:39:45 -0400

    Update ldi_tb.v

--- ldi_tb.v
+++ ldi_tb.v
@@ -6,9 +6,8 @@ module ldi_tb;
     reg clock, clear, MD_read, MAR_clear;
 //    reg [31:0] Mdatain;
 
-    parameter   Default = 5'b00000, T0 = 5'b00001, T1 = 5'b00010, T2 = 5'b00011, T3 = 5'b00100, 
-	 T4 = 5'b00101, T5 = 5'b00110, T6 = 5'b00111, T7 = 5'b01000, T8 = 5'b01001, T9 = 5'b01010, T10 = 5'b01011,
-	 T11 = 5'b01100, T12 = 5'b01101, T13 = 5'b01110, T14 = 5'b01111, T15 = 5'b10000, T16 = 5'b10001, T17 = 5'b10010; 
+    parameter   Default = 4'b0000, T0 = 4'b0001, T1 = 4'b0010, T2 = 4'b0011, T3 = 4'b0100, 
+	 T4 = 4'b0101, T5 = 4'b0110, T6 = 4'b0111, T7 = 4'b1000, T8 = 4'b1001; 
     reg [3:0] Present_state = Default;
 	 
 	 reg Csignout, Grb, Gra, Read, MARin, Rin, BAout; 
@@ -18,7 +17,7 @@ module ldi_tb;
 		     .MDRin(MDRin), .MAR_clear(MAR_clear),
 		     .PCin(PCin), .MD_read(MD_read),
 		     .Zlowin(Zlowin), .Zhighin(Zhighin), .Zlowout(Zlowout), .IncPC(IncPC), .Yin(Yin), .IRin(IRin),  
-		   .MDRout(MDRout), .PCout(PCout),
+		     .MDRout(MDRout), 
 			  
 			  .Csignout(Csignout), .Grb(Grb), .Gra(Gra), .BAout(BAout), .Read(Read), .MARin(MARin), .Rin(Rin), .ADD(ADD));
 // add test logic here
@@ -42,15 +41,6 @@ always @(posedge clock) // finite state machine; if clock rising-edge
 				T5 : #40 Present_state = T6;
             T6 : #40 Present_state = T7;
             T7 : #40 Present_state = T8;
-				T8 : #40 Present_state = T9;
-				T9 : #40 Present_state = T10;
-				T10 : #40 Present_state = T11;
-				T11 : #40 Present_state = T12;
-				T12 : #40 Present_state = T13;
-				T13 : #40 Present_state = T14;
-				T14 : #40 Present_state = T15;
-				T15 : #40 Present_state = T16;
-				T16 : #40 Present_state = T17;
         endcase
     end
 
@@ -63,23 +53,20 @@ always @(Present_state)
                     PCin <=0; MDRin <= 0; IRin <= 0; Yin <= 0;
                     IncPC <= 0; Read <= 0; ADD <= 0;
                      MD_read <= 0;
-						  Csignout <= 0; Grb <= 0; Gra <= 0; BAout <= 0; Rin <= 0; MAR_clear <= 1;
+						  Csignout <= 0; Grb <= 0; Gra <= 0; BAout <= 0; Rin <= 0; MAR_clear <= 0;
             end
-				T0: begin //Puts PC into MAR S
+		
+            T0: begin //Puts PC into MAR S
 						#10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1; 
 						#15 PCout <= 0; MARin <= 0; IncPC <= 0; Zlowin <= 0;
             end
             T1: begin //Puts ram data into Mdatain
-						#10 Zlowout <= 1; PCin <= 1; Read <= 1;  
-						#15 Zlowout <= 0; PCin <= 0; Read <= 0; 
+						#10 Zlowout <= 1; PCin <= 1; Read <= 1;  MD_read <= 1; MDRin <= 1; 
+						#15 Zlowout <= 0; PCin <= 0; Read <= 0;  MD_read <= 0; MDRin <= 0;
             end
             T2: begin //MDR content on to bus
-                  #10 MDRout <= 1; MD_read <= 1; MDRin <= 1; 
-						#15 MDRout <= 0; MD_read <= 0; MDRin <= 0;
-            end
-				T3: begin //IR has opcode 
-                  #10 IRin <= 1;  
-						#15 IRin <= 0; 
+                  #10 MDRout <= 1; IRin <= 1;
+						#15 MDRout <= 0; IRin <= 0;
             end
             T4: begin //Yin contains 0 from R0
                   #10 Grb <= 1; BAout <= 1; Yin <= 1; 
@@ -90,37 +77,7 @@ always @(Present_state)
 						#15 Csignout <= 0; ADD <= 0; Zlowin <= 0; 
             end
 				T6: begin
-						#10 Zlowout <= 1; Gra <= 1; Rin <= 1; //In register 1
-						#15 Zlowout <= 0; Gra <= 0; Rin <= 0; 
-				end
-				
-				
-            T7: begin //Puts PC into MAR 
-						#10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1; MAR_clear <= 0;
-						#15 PCout <= 0; MARin <= 0; IncPC <= 0; Zlowin <= 0;
-            end
-            T8: begin //Puts ram data into Mdatain
-						#10 Zlowout <= 1; PCin <= 1; Read <= 1; 
-						#15 Zlowout <= 0; PCin <= 0; Read <= 0; 
-            end
-            T9: begin //MDR content on to bus
-                  #10  MD_read <= 1; MDRin <= 1; 
-						#15  MD_read <= 0; MDRin <= 0;
-            end
-				T10: begin //IR has opcode 
-                  #10 IRin <= 1; MDRout <= 1;
-						#15 IRin <= 0; MDRout <= 0;
-            end
-            T11: begin //Yin contains 0 from R0
-                  #10 Grb <= 1; BAout <= 1; Yin <= 1; 
-						#15 Grb <= 0; BAout <= 0; Yin <= 0; 
-            end
-            T12: begin
-                  #10 Csignout <= 1; ADD <= 1; Zlowin <= 1; //contents in Creg + Yreg = ZlowReg
-						#15 Csignout <= 0; ADD <= 0; Zlowin <= 0; 
-            end
-				T13: begin
-						#10 Zlowout <= 1; Gra <= 1; Rin <= 1; //In regiser R2
+						#10 Zlowout <= 1; Gra <= 1; Rin <= 1; //In register 2
 						#15 Zlowout <= 0; Gra <= 0; Rin <= 0; 
 				end
         endcase
