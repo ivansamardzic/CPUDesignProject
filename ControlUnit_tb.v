5ad4d7602cdb738f3704f134c94240906b5df3b8
Ivan Samardzic <ivan.samardzic7@gmail.com>
2024-03-25 12:14:32 -0400

    Should work

--- ControlUnit_tb.v
+++ ControlUnit_tb.v
@@ -1,1 +1,17 @@
+`timescale 1ns/10ps
+module ControlUnit_tb;
 
+	reg clock, reset, stop;
+	reg [31:0] INPUT_UNIT;
+	wire [31:0] OUTPUT_UNIT;
+	
+	DataPath DUT(.clock(clock), .reset(reset), .stop(stop), .INPUT_UNIT(INPUT_UNIT), .OUTPUT_UNIT(OUTPUT_UNIT));
+						
+    initial
+		begin
+		    clock = 0;
+		forever #10 clock = ~ clock;
+		end
+
+endmodule
+	
