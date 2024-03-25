ac6544ff9b6f614600c40708546fe8dbeb94d421
TaesonTitus <114195814+TaesonTitus@users.noreply.github.com>
2024-03-25 14:06:22 -0400

    Update IO_tb.v
    T

--- IO_tb.v
+++ IO_tb.v
@@ -67,7 +67,7 @@ always @(Present_state)
                     PCin <=0; MDRin <= 0; IRin <= 0; Yin <= 0;
                     IncPC <= 0; Read <= 0; OR <= 0;
                     MD_read <= 0; Rout <= 0; Strobe <= 0; Out_Portin <= 0; InPortout <= 0; 
-						  Csignout <= 0; Grb <= 0; Gra <= 0; BAout <= 0; Rin <= 0; MAR_clear <= 1;
+						  Csignout <= 0; Grb <= 0; Gra <= 0; BAout <= 0; Rin <= 0; MAR_clear <= 0;
             end
 				
 				//IN 
@@ -76,16 +76,12 @@ always @(Present_state)
 						#15 PCout <= 0; MARin <= 0; IncPC <= 0; Zlowin <= 0;
             end
             T1: begin //Puts ram data into Mdatain
-						#10 Zlowout <= 1; PCin <= 1; Read <= 1;  
-						#15 Zlowout <= 0; PCin <= 0; Read <= 0; 
+						#10 Zlowout <= 1; PCin <= 1; Read <= 1;  MD_read <= 1; MDRin <= 1; 
+						#15 Zlowout <= 0; PCin <= 0; Read <= 0;  MD_read <= 0; MDRin <= 0;
             end
             T2: begin //MDR content on to bus
-                  #10 MDRout <= 1; MD_read <= 1; MDRin <= 1; 
-						#15 MDRout <= 0; MD_read <= 0; MDRin <= 0;
-            end
-				T3: begin //IR has opcode 
-                  #10 IRin <= 1;  
-						#15 IRin <= 0; 
+                  #10 MDRout <= 1; IRin <= 1;
+						#15 MDRout <= 0; IRin <= 0;
             end
             T4: begin //Yin contains 0 from R0
                   #10 INPUT_UNIT <= 32'h13868904; Strobe <= 1; 
@@ -98,21 +94,17 @@ always @(Present_state)
 				
 				// OUT 
 				T6: begin //Puts PC into MAR S
-						#10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1; MAR_clear <= 0;
+						#10 PCout <= 1; MARin <= 1; IncPC <= 1; Zlowin <= 1; 
 						#15 PCout <= 0; MARin <= 0; IncPC <= 0; Zlowin <= 0;
             end
             T7: begin //Puts ram data into Mdatain
-						#10 Zlowout <= 1; PCin <= 1; Read <= 1;  
-						#15 Zlowout <= 0; PCin <= 0; Read <= 0; 
+						#10 Zlowout <= 1; PCin <= 1; Read <= 1;  MD_read <= 1; MDRin <= 1; 
+						#15 Zlowout <= 0; PCin <= 0; Read <= 0;  MD_read <= 0; MDRin <= 0;
             end
             T8: begin //MDR content on to bus
-                  #10 MDRout <= 1; MD_read <= 1; MDRin <= 1; 
-						#15 MDRout <= 0; MD_read <= 0; MDRin <= 0;
+                  #10 MDRout <= 1; IRin <= 1;
+						#15 MDRout <= 0; IRin <= 0;
             end
-				T9: begin //IR has opcode 
-                  #10 IRin <= 1;  
-						#15 IRin <= 0; 
-				end
 				T10: begin
                   #10 Gra <= 1; Rout <= 1; Out_Portin <= 1; 
 						#15 Gra <= 0; Rout <= 0; Out_Portin <= 0; 
